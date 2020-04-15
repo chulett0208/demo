64c00172dd956d3f1318cf4e202b9e47b5b487a9
Charles <charles.hulett@bnl-consulting.com>
2020-04-15 17:00:52 -0400

    Add all files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const element = (
+  <div>
+    <h1>Cats</h1>
+    <nav>
+      <b>Home &nbsp; Types</b>
+    </nav>
+  </div>
+);
+
+//var element = React.createElement('h1', { className: 'greeting' }, 'Cats');
+//var element = React.createElement('ul')
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
