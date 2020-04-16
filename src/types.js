69a82daf03d32a65b680b09a408e6a3b15b53750
Charles <charles.hulett@bnl-consulting.com>
2020-04-16 15:46:39 -0400

    types.js created

---
+++ src/types.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const element = (
+  <div>
+    <h1>Types</h1>
+    <nav>
+      <b><a href = "index.js"></a>Home &nbsp; <a href = "types.js"></a>Types</b>
+    </nav>
+  </div>
+);
