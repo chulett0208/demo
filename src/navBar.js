0499cb9bcd2e5962275705fc553b1d59954d3506
Charles <charles.hulett@bnl-consulting.com>
2020-05-13 20:49:08 -0400

    navbar/content.js/index.css all updated

--- src/navBar.js
+++ src/navBar.js
@@ -2,30 +2,28 @@ import React from 'react';
 
 function navBar() {
     return(
-        <nav class="navbar navbar-custom fixed-top" style={{backgroundColor: "#292929"}}>
+        <nav class="main-nav fixed-top">
             
-            <div class="container-fluid">
-                
-                <div class="navbar-header">
-                    <img src="https://bnlconsulting.com/bnl.jpg" width="130" 
-                    alt="" class="inline align-middle mr-2"/>
-                </div>
+            <div class="navbar-header">
+                <img src="https://bnlconsulting.com/bnl.jpg" 
+                alt="" class="inline align-middle mr-2"/>
+            </div>
 
-                <ul class="nav navbar-nav-right" style={{color: "#808080"}}>
-                    <li>
-                        <a href="/" class="nav-link" style={{color: "#808080"}}>Home</a>
-                    </li>
-                    <li>
-                        <a href="portfolio" class="nav-link" style={{color: "#808080"}}>Portfolio</a>
-                    </li>
-
-                    <li>
-                        <a href="services" class="nav-link" style={{color: "#808080"}}>Services&nbsp;</a>
-                    </li>
-                    <li><a href="about" class="nav-link" style={{color: "#808080"}}>About</a></li>
-                    <li><a href="blog" class="nav-link" style={{color: "#808080"}}>Blog</a></li>
-                    <li><a href="contact" class="nav-link" style={{color: "#808080"}}>Contact</a></li>
-                </ul>
+            <div class="main-menu">
+            <ul class="navbar-nav-right">
+                <li>
+                    <a href="/" class="nav-link">Home</a>
+                </li>
+                <li>
+                    <a href="portfolio" class="nav-link">Portfolio</a>
+                </li>
+                <li>
+                    <a href="services" class="nav-link">Services&nbsp; <i class="fa fa-caret-down"></i></a>
+                </li>
+                <li><a href="about" class="nav-link">About</a></li>
+                <li><a href="blog" class="nav-link">Blog</a></li>
+                <li><a href="contact" class="nav-link">Contact</a></li>
+            </ul>
             </div>
         </nav>
     );
--- src/navbar2.js
+++ src/navbar2.js
@@ -29,3 +29,41 @@ function navBar2() {
     );
 }
 export default navBar2;
+
+
+
+import React from 'react';
+
+function navBar() {
+    return(
+        <div class="container">
+        <nav class="navbar navbar-custom fixed-top" style={{backgroundColor: "#292929"}}>
+            
+            <div class="container-fluid">
+                
+                <div class="navbar-header">
+                    <img src="https://bnlconsulting.com/bnl.jpg" width="130" 
+                    alt="" class="inline align-middle mr-2"/>
+                </div>
+
+                <ul class="nav navbar-nav-right" style={{color: "#808080"}}>
+                    <li>
+                        <a href="/" class="nav-link" style={{color: "#808080"}}>Home</a>
+                    </li>
+                    <li>
+                        <a href="portfolio" class="nav-link" style={{color: "#808080"}}>Portfolio</a>
+                    </li>
+
+                    <li>
+                        <a href="services" class="nav-link" style={{color: "#808080"}}>Services&nbsp; <i class="fa fa-caret-down"></i></a>
+                    </li>
+                    <li><a href="about" class="nav-link" style={{color: "#808080"}}>About</a></li>
+                    <li><a href="blog" class="nav-link" style={{color: "#808080"}}>Blog</a></li>
+                    <li><a href="contact" class="nav-link" style={{color: "#808080"}}>Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    );
+}
+export default navBar;
