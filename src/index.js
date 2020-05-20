3ef9f6da92dddc14bc9f0de262c47546e83d3592
Charles <charles.hulett@bnl-consulting.com>
2020-05-20 11:44:31 -0400

    trying to delete service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*import * as serviceWorker from './serviceWorker';*/
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from 'react-router-dom';
 
@@ -12,4 +12,4 @@ ReactDOM.render(
   <App />
   </BrowserRouter>, document.getElementById('root'));
 
-serviceWorker.unregister();
+/*serviceWorker.unregister();*/
