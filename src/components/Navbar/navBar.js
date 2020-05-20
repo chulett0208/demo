5121ec383612ac8ad473d5d5812aaf5dfc8b298a
Charles <charles.hulett@bnl-consulting.com>
2020-05-20 14:12:03 -0400

    updates

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -10,35 +10,35 @@ function navBar() {
 });*/
     return(
         <div>
-        <div className="menu-btn">
-            <i className="fas fa-bars"></i>
-        </div>
+            <div className="menu-btn">
+                <i className="fas fa-bars"></i>
+            </div>
         
-        <nav className="main-nav fixed-top">
+            <nav className="main-nav fixed-top">
             
-            <div className="navbar-header">
-                <img src="https://bnlconsulting.com/bnl.jpg" 
-                alt="" className="inline align-middle mr-2"/>
-            </div>
+                <div className="navbar-header">
+                    <img src="https://bnlconsulting.com/bnl.jpg" 
+                    alt="" className="inline align-middle mr-2"/>
+                </div>
 
-            <div className="main-menu">
-                <ul className="navbar-nav-right">
-                    <li>
-                        <a href="/" className="nav-link">Home</a>
-                    </li>
-                    <li>
-                        <a href="portfolio" className="nav-link">PORTFOLIO</a>
-                    </li>
-                    <li>
-                        <a href="services" className="nav-link">SERVICES&nbsp; <i class="fa fa-caret-down"></i></a>
-                    </li>
-                    <li><a href="about" className="nav-link">ABOUT</a></li>
-                    <li><a href="blog" className="nav-link">BLOG</a></li>
-                    <li><a href="contact" className="nav-link">CONTACT</a></li>
-                </ul>
-            </div>
+                <div className="main-menu">
+                    <ul className="navbar-nav-right">
+                        <li>
+                            <a href="/" className="nav-link">Home</a>
+                        </li>
+                        <li>
+                            <a href="portfolio" className="nav-link">PORTFOLIO</a>
+                        </li>
+                        <li>
+                            <a href="services" className="nav-link">SERVICES&nbsp; <i class="fa fa-caret-down"></i></a>
+                        </li>
+                        <li><a href="about" className="nav-link">ABOUT</a></li>
+                        <li><a href="blog" className="nav-link">BLOG</a></li>
+                        <li><a href="contact" className="nav-link">CONTACT</a></li>
+                    </ul>
+                </div>
             
-        </nav>
+            </nav>
         </div>
     );
 }
