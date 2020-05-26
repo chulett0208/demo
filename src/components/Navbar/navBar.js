bd0e405a04e1ab50fa9d65b58990b0ca39a41685
Charles <charles.hulett@bnl-consulting.com>
2020-05-26 13:04:55 -0400

    not needed code deleted / navbar updated

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import '../../index.css';
 function navBar() {
-    /*(window).scroll(function() {
-  if ((document).scrollTop() > 50) {
-    ('nav').addClass('shrink');
-  } else {
-    ('nav').removeClass('shrink');
-  }
-});*/
+
     return(
 
         <div className="navContainer">
@@ -23,7 +17,7 @@ function navBar() {
                 </button>
                 
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="nav navbar-nav navbar-right">
+                    <ul className="nav navbar-nav">
                         
                         <li className="nav-item">
                             <a href="/" className="nav-link">Home</a>
@@ -57,28 +51,3 @@ function navBar() {
     );
 }
 export default navBar;
-
-
-
-/*<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>*/
