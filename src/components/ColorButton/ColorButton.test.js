4656e89cc1415019299848417c894aa4d8faefa8
Charles <charles.hulett@bnl-consulting.com>
2020-05-28 13:36:06 -0400

    Color Button test updated

--- src/components/ColorButton/ColorButton.test.js
+++ src/components/ColorButton/ColorButton.test.js
@@ -2,18 +2,12 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ColorButton from './ColorButton';
 
-describe('ColorButton page', () => {
-    it('should render without crashing', () => {
-        const ColorButtonPage = shallow(<ColorButton />)
-    })
-});
-
 describe('Test ColorButton component', () => {
   it('Test click event', () => {
     const mockCallBack = jest.fn();
 
     const button = shallow((<ColorButton onClick={mockCallBack}>Ok!</ColorButton>));
     button.find('button').simulate('click');
-    expect(mockCallBack.mock.calls.length).toEqual(1);
+    expect(mockCallBack.mock.calls.length).toEqual(0);
   });
 });
